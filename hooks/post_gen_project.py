fc809421b38120963981974b74c1b224dbd054cb
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2023-05-20 15:10:50 +0300

    docs: imrpove docs and makefile (#15)
    Co-authored-by: Ran Isenberg <ran.isenberg@ranthebuilder.cloud>

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,26 @@
+import os
+from pathlib import Path 
+import subprocess
+
 def main():
+    print("Running 'git init'")
+    subprocess.run(['git', 'init'], check=True)
+
+    print("Running 'pip install --upgrade pip pre-commit poetry'")
+    subprocess.run(['pip', 'install', '--upgrade', 'pip', 'pre-commit', 'poetry'], check=True)
+
+    print("Running 'pre-commit install'")
+
+    subprocess.run(['pre-commit', 'install'], check=True)
+
+    print("Running 'poetry config --local virtualenvs.in-project true'")
+    subprocess.run(['poetry', 'config', '--local', 'virtualenvs.in-project', 'true'], check=True)
+
+    print("Running 'poetry install'")
+    subprocess.run(['poetry', 'install'], check=True)
+
     print("Project successfully initialized")
+    return
 
 if __name__ == "__main__":
     main()
