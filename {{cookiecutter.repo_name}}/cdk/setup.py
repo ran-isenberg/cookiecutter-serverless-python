21fc5f0e2658dc26d7f834a932f7fb5a3b6461ca
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2024-11-14 21:21:40 +0200

    feature: sync to Cookbook v6.0.0 python 3.13 (#28)
    Co-authored-by: Ran Isenberg <ran.isenberg@ranthebuilder.cloud>

--- {{cookiecutter.repo_name}}/cdk/setup.py
+++ {{cookiecutter.repo_name}}/cdk/setup.py
@@ -6,20 +6,20 @@ from setuptools import find_packages, setup
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='{{cookiecutter.service_name}}-cdk',
-    version='3.1',
-    description='CDK code for deploying the serverless service',
+    name="{{cookiecutter.service_name}}-cdk",
+    version="3.1",
+    description="CDK code for deploying the serverless service",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.12',
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.13",
     ],
-    url='https://github.com/ran-isenberg/aws-lambda-handler-cookbook',
-    author='{{cookiecutter.author}}',
-    author_email='{{cookiecutter.email}}',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    package_data={'': ['*.json']},
+    url="https://github.com/ran-isenberg/aws-lambda-handler-cookbook",
+    author="{{cookiecutter.author}}",
+    author_email="{{cookiecutter.email}}",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    package_data={"": ["*.json"]},
     include_package_data=True,
-    python_requires='>=3.12',
+    python_requires=">=3.13",
     install_requires=[],
 )
