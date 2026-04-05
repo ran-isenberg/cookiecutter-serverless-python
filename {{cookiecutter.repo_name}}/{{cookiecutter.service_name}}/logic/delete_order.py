cf99297778705230da9d3302c939f3beadf3c2d8
Ran Isenberg <60175085+ran-isenberg@users.noreply.github.com>
2026-04-05 11:32:52 +0300

    feature: align to 9.6.0 (#35)
    Co-authored-by: Ran Isenberg <ran.isenberg@ranthebuilder.cloud>

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.service_name}}/logic/delete_order.py
@@ -0,0 +1,13 @@
+from {{cookiecutter.service_name}}.dal import get_dal_handler
+from {{cookiecutter.service_name}}.dal.db_handler import DalHandler
+from {{cookiecutter.service_name}}.handlers.utils.observability import logger, tracer
+from {{cookiecutter.service_name}}.models.output import DeleteOrderOutput
+
+
+@tracer.capture_method(capture_response=False)
+def delete_order(order_id: str, table_name: str) -> DeleteOrderOutput:
+    logger.info('starting to handle delete order request', order_id=order_id)
+
+    dal_handler: DalHandler = get_dal_handler(table_name)
+    dal_handler.delete_order_from_db(order_id)
+    return DeleteOrderOutput(order_id=order_id)
